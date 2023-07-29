be5edcf9c5096947fb9717fdca02a9d2639ffb0a
Shyan <shyan.inc@gmail.com>
2023-07-29 19:53:19 +0300

    Add Users, Roles Modules & Add Users, Roles, UserRoles Models

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { RolesModule } from './roles/roles.module';
+import { AuthModule } from './auth/auth.module';
 import * as process from 'process';
+import { User } from './users/users.model';
+import { Role } from './roles/roles.model';
+import { UserRoles } from './roles/user-roles.model';
+import { MoviesModule } from './movies/movies.module';
+import { SeriesModule } from './series/series.module';
 
 @Module({
   controllers: [AppController],
@@ -19,9 +27,15 @@ import * as process from 'process';
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
-      models: [],
+      models: [User, Role, UserRoles],
       autoLoadModels: true,
+      synchronize: true,
     }),
+    UsersModule,
+    RolesModule,
+    AuthModule,
+    MoviesModule,
+    SeriesModule,
   ],
 })
 export class AppModule {}
