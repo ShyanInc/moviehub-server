ba357fff82adb373b3640b34e3962660fbbf1b91
Shyan <shyan.inc@gmail.com>
2023-07-29 19:09:11 +0300

    Configure .env & configure postgres connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
+import * as process from 'process';
 
 @Module({
-  imports: [],
   controllers: [AppController],
   providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env`,
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.POSTGRES_PORT,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      models: [],
+      autoLoadModels: true,
+    }),
+  ],
 })
 export class AppModule {}
