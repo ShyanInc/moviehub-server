db62dc445cf32a9826c40de965b07d51687fb5fa
Shyan <shyan.inc@gmail.com>
2023-08-05 23:20:47 +0300

    Document Roles Controller & Document create-role.dto.ts

--- src/roles/dto/create-role.dto.ts
+++ src/roles/dto/create-role.dto.ts
@@ -1,4 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateRoleDto {
+  @ApiProperty({ example: 'User', description: 'Role value' })
   value: string;
+  @ApiProperty({ example: 'User role', description: 'Role description' })
   description: string;
 }
--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,26 +1,37 @@
 import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { CreateRoleDto } from './dto/create-role.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Role } from './roles.model';
 
+@ApiTags('Roles')
 @Controller('roles')
 export class RolesController {
   constructor(private rolesService: RolesService) {}
 
+  @ApiOperation({ summary: 'Create role' })
+  @ApiResponse({ status: 201, type: Role })
   @Post()
   create(@Body() dto: CreateRoleDto) {
     return this.rolesService.create(dto);
   }
 
+  @ApiOperation({ summary: 'Get all roles' })
+  @ApiResponse({ status: 200, type: [Role] })
   @Get()
   getAll() {
     return this.rolesService.getAllRoles();
   }
 
+  @ApiOperation({ summary: 'Get role by value' })
+  @ApiResponse({ status: 200, type: Role })
   @Get('/:value')
   getByValue(@Param('value') value: string) {
     return this.rolesService.getRoleByValue(value);
   }
 
+  @ApiOperation({ summary: 'Delete role by id' })
+  @ApiResponse({ status: 204 })
   @Delete('/:id')
   delete(@Param('id') id: number) {
     return this.rolesService.deleteRoleById(id);
