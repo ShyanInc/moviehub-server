c4f4908a17488e9e0a9194dc390434e877626097
Shyan <shyan.inc@gmail.com>
2023-08-07 00:00:15 +0300

    Document Movies Controller

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -10,26 +10,36 @@ import {
 import { MoviesService } from './movies.service';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
+import { ApiOperation, ApiProperty, ApiResponse } from '@nestjs/swagger';
+import { Movie } from './movies.model';
 
 @Controller('movies')
 export class MoviesController {
   constructor(private moviesService: MoviesService) {}
 
+  @ApiOperation({ summary: 'Get all movies' })
+  @ApiResponse({ status: 200, type: [Movie] })
   @Get()
   getAll() {
     return this.moviesService.getAllMovies();
   }
 
+  @ApiOperation({ summary: 'Get movie by id' })
+  @ApiResponse({ status: 200, type: Movie })
   @Get('/:id')
   getById(@Param('id') id: number) {
     return this.moviesService.getMovieById(id);
   }
 
+  @ApiOperation({ summary: 'Create movie' })
+  @ApiResponse({ status: 201, type: Movie })
   @Post()
   create(@Body() dto: CreateMovieDto) {
     return this.moviesService.createMovie(dto);
   }
 
+  @ApiOperation({ summary: 'Delete movie' })
+  @ApiResponse({ status: 204 })
   @Delete('/:id')
   deleteById(@Param('id') id: number) {
     return this.moviesService.deleteMovieById(id);
