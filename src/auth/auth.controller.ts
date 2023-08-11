5b71ef37c006d9eb91eff6eb4e1e718b81b3bcb4
Shyan <Shyan.Inc@gmail.com>
2023-08-12 00:14:26 +0300

    Document Auth Controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,46 @@
 import { Controller, Body, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiOperation,
+  ApiProperty,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { AuthService } from './auth.service';
+import { HttpCode } from '@nestjs/common/decorators';
+
+class JwtToken {
+  @ApiProperty({
+    example:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+    description: 'JsonWebToken',
+  })
+  token: string;
+}
 
 @ApiTags('Authorization')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @ApiOperation({ summary: 'Login' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return a JsonWebToken',
+    type: JwtToken,
+  })
   @Post('/login')
+  @HttpCode(200)
   login(@Body() dto: CreateUserDto) {
     return this.authService.login(dto);
   }
 
+  @ApiOperation({ summary: 'Register' })
+  @ApiResponse({
+    status: 201,
+    description: 'Return a JsonWebToken',
+    type: JwtToken,
+  })
   @Post('/register')
   register(@Body() dto: CreateUserDto) {
     return this.authService.register(dto);
