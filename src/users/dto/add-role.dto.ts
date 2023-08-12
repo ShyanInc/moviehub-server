72651db47cae990602533899589c73867a23b033
Shyan <shyan.inc@gmail.com>
2023-08-12 23:45:59 +0300

    Add Validation Pipe

--- src/users/dto/add-role.dto.ts
+++ src/users/dto/add-role.dto.ts
@@ -1,8 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString } from 'class-validator';
 
 export class AddRoleDto {
   @ApiProperty({ example: 1, description: 'User ID' })
+  @IsNumber({}, { message: 'Have to be number' })
   userId: number;
   @ApiProperty({ example: 'User', description: 'Role value' })
+  @IsString({ message: 'Have to be string' })
   value: string;
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,11 +1,18 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, Length } from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty({
     example: 'email@gmail.com',
     description: 'New user email address',
   })
+  @IsString({ message: 'Have to be string' })
+  @IsEmail({}, { message: 'Invalid email' })
   email: string;
   @ApiProperty({ example: 'pass123', description: 'New user password' })
+  @IsString({ message: 'Have to be string' })
+  @Length(4, 32, {
+    message: 'Password min length is 4 and max is 32 characters',
+  })
   password: string;
 }
